26ebbec306366963eba40b92422f8e3ee3b32985
robbie <robbiekruszynski@gmail.com>
2023-11-18 23:41:34 +0300

    pkg for modal

--- src/layout/MainLayout/Header/ProfileSection/index.js
+++ src/layout/MainLayout/Header/ProfileSection/index.js
@@ -1,91 +1,88 @@
-import { useState, useRef, useEffect } from 'react';
-
+// ProfileSection.js
+import React, { useState, useRef, useEffect } from 'react';
+import { ethers } from 'ethers';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
-// material-ui
 import { useTheme } from '@mui/material/styles';
 import {
-  Avatar,
-  Box,
-  Card,
-  CardContent,
-  Chip,
-  ClickAwayListener,
-  Divider,
-  Grid,
-  InputAdornment,
-  List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  OutlinedInput,
-  Paper,
-  Popper,
-  Stack,
-  Switch,
-  Typography
+    Avatar,
+    Box,
+    Card,
+    CardContent,
+    Chip,
+    ClickAwayListener,
+    Divider,
+    Grid,
+    InputAdornment,
+    List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    OutlinedInput,
+    Paper,
+    Popper,
+    Stack,
+    Switch,
+    Typography
 } from '@mui/material';
-
-// third-party
 import PerfectScrollbar from 'react-perfect-scrollbar';
-
-// project imports
 import MainCard from 'ui-component/cards/MainCard';
 import Transitions from 'ui-component/extended/Transitions';
 import UpgradePlanCard from './UpgradePlanCard';
 import User1 from 'assets/images/users/zkLumi.png';
-
-// assets
 import { IconLogout, IconSearch, IconSettings, IconUser } from '@tabler/icons';
 
-// ==============================|| PROFILE MENU ||============================== //
 
 const ProfileSection = () => {
-  const theme = useTheme();
-  const customization = useSelector((state) => state.customization);
-  const navigate = useNavigate();
+    const theme = useTheme();
+    const customization = useSelector((state) => state.customization);
+    const navigate = useNavigate();
 
-  const [sdm, setSdm] = useState(true);
-  const [value, setValue] = useState('');
-  const [notification, setNotification] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-  const [open, setOpen] = useState(false);
-  /**
-   * anchorRef is used on different componets and specifying one type leads to other components throwing an error
-   * */
-  const anchorRef = useRef(null);
-  const handleLogout = async () => {
-    console.log('Logout');
+    const [sdm, setSdm] = useState(true);
+    const [value, setValue] = useState('');
+    const [notification, setNotification] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    const [open, setOpen] = useState(false);
+
+    const anchorRef = useRef(null);
+
+    const handleWalletConnect = async () => {
+      // Implement wallet connection logic here
+      console.log('Connect to wallet');
   };
 
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-    setOpen(false);
-  };
+    const handleLogout = async () => {
+        console.log('Logout');
+    };
 
-  const handleListItemClick = (event, index, route = '') => {
-    setSelectedIndex(index);
-    handleClose(event);
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+        setOpen(false);
+    };
 
-    if (route && route !== '') {
-      navigate(route);
-    }
-  };
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
+    const handleListItemClick = (event, index, route = '') => {
+        setSelectedIndex(index);
+        handleClose(event);
+        if (route && route !== '') {
+            navigate(route);
+        }
+    };
 
-  const prevOpen = useRef(open);
-  useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
 
-    prevOpen.current = open;
-  }, [open]);
+ 
+
+    const prevOpen = useRef(open);
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+        prevOpen.current = open;
+    }, [open]);
 
   return (
     <>
