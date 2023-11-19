15cb43b0d02fc9a53dceaf6c540242fd7bdc0149
robbie <robbiekruszynski@gmail.com>
2023-11-19 04:12:32 +0300

    update dash

--- src/views/dashboard/Default/FinalizeButtonsCard.js
+++ src/views/dashboard/Default/FinalizeButtonsCard.js
@@ -1,55 +1,59 @@
-// import PropTypes from 'prop-types';
-// import { useState, useEffect } from 'react';
-// import { useSelector } from 'react-redux';
-
-// material-ui
-// import { useTheme } from '@mui/material/styles';
+import React from 'react';
 import { Grid, Typography, Box, Button } from '@mui/material';
-// import { Grid, Box, Button } from '@mui/material';
-
-// import MethodFastSpeed from './MethodFastSpeed';
-// import MethodNormalSpeed from './MethodNormalSpeed';
-// third-party
-// import ApexCharts from 'apexcharts';
-// import Chart from 'react-apexcharts';
-
-// project imports
-// import SkeletonTotalGrowthBarChart from 'ui-component/cards/Skeleton/TotalGrowthBarChart';
-// import MainCard from 'ui-component/cards/MainCard';
+import { ethers } from 'ethers';
 import { gridSpacing } from 'store/constant';
 
-
-
-// ==============================|| DASHBOARD DEFAULT - TOTAL GROWTH BAR CHART ||============================== //
-
 const FinalizeButtonsCard = () => {
-  // const [value, setValue] = useState('today');
-  // const theme = useTheme();
-  // const customization = useSelector((state) => state.customization);
+  // Function to handle the bridge operation
+  const handleBridge = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+
+        // Example transaction - Replace with your actual transaction details
+        const transaction = {
+          to: address, // Assuming sending to the user's address for demonstration
+          value: ethers.utils.parseEther("0.01"), // Example amount - 0.01 ETH
+        };
+
+        // Prompt user to confirm the transaction
+        const txResponse = await signer.sendTransaction(transaction);
+        console.log('Transaction response:', txResponse);
+      } else {
+        console.error('Ethereum object not found, install MetaMask.');
+      }
+    } catch (error) {
+      console.error('Error during the transaction:', error);
+    }
+  };
 
   return (
     <>
       <Box mb={1} pt={1}>
-        <Typography component="h3" variant="h4" >Confirm Transfer</Typography>
-    </Box>
-        <Grid container spacing={gridSpacing}>
-          <Grid item xs={12}>
-            <Grid container spacing={gridSpacing}>
-              <Grid item xs={12} >
-                <Button variant="contained" size="large" color="primary" sx={{width: '100%',pt:2,pb:2}}>Bridge</Button>
-              </Grid>
-              {/* <Grid item xs={12} >
-                <MethodFastSpeed isLoading={isLoading} />
-              </Grid> */}
+        <Typography component="h3" variant="h4">Confirm Transfer</Typography>
+      </Box>
+      <Grid container spacing={gridSpacing}>
+        <Grid item xs={12}>
+          <Grid container spacing={gridSpacing}>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                sx={{ width: '100%', pt: 2, pb: 2 }}
+                onClick={handleBridge} // Call handleBridge when the button is clicked
+              >
+                Bridge
+              </Button>
             </Grid>
           </Grid>
+        </Grid>
       </Grid>
     </>
   );
 };
 
-// FinalizeButtonsCard.propTypes = {
-//   isLoading: PropTypes.bool
-// };
-
 export default FinalizeButtonsCard;
+
